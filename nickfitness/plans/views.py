31dd1053c641b2b93f7d6a5b6c8b691d30d3c647
RichardHoangHuy <baohuyhoang.work@gmail.com>
2019-12-16 15:32:09 +1100

    Adding price and coupon

--- nickfitness/plans/views.py
+++ nickfitness/plans/views.py
@@ -5,6 +5,8 @@ from django.views import generic
 from .models import FitnessPlan
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+import stripe
+stripe.api_key = 'sk_test_nuWJOw3lDoNZiEuwZONJlMwJ00eFdh2VCg'
 
 def home(request):
     plans = FitnessPlan.objects
@@ -12,20 +14,47 @@ def home(request):
 
 def plan(request,pk):
     plan = get_object_or_404(FitnessPlan, pk=pk)
-    if plan.premium :
+    if plan.premium:
         return redirect('join')
     else:
-        return render(request, 'plans/plan.html', {'plan':plan})
+        return render(request, 'plans/plan.html', {'plan': plan})
 
 def join(request):
     return render(request, 'plans/join.html')
 
 @login_required
 def checkout(request):
+
+    coupons = {'halloween': 31, 'welcome': 10}
+
     if request.method == 'POST':
         return redirect('home')
     else:
-        return render(request, 'plans/checkout.html')
+        coupon = 'none'
+        plan = 'monthly'
+        price = 1000
+        og_dollar = 10
+        coupon_dollar = 0
+        final_dollar = 10
+        if request.method == 'GET' and 'plan' in request.GET:
+            if request.GET['plan'] == 'yearly':
+                plan = 'yearly'
+                price = 10000
+                og_dollar = 100
+                final_dollar = 100
+        if request.method == 'GET' and 'coupon' in request.GET:
+            if request.GET['coupon'].lower() in coupons:
+                coupon = request.GET['coupon'].lower()
+                percentage = coupons[request.GET['coupon'].lower()]
+                coupon_price = int((percentage / 100) * price)
+                price = price - coupon_price
+                coupon_dollar = str(coupon_price)[
+                    :-2] + '.' + str(coupon_price)[-2:]
+                final_dollar = str(price)[:-2] + '.' + str(price)[-2:]
+                
+        return render(request, 'plans/checkout.html',
+        {'plan':plan,'coupon':coupon,'price':price,'og_dollar':og_dollar,
+        'coupon_dollar':coupon_dollar,'final_dollar':final_dollar})
 
 def settings(request):
     return render(request, 'registration/settings.html')
